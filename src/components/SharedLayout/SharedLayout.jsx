36623222323d23dda0ae2833f583811edffec7cc
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-05-30 12:11:27 +0300

    correcting suspanse component

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,12 +2,13 @@ import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { Suspense } from 'react';
 import { Navigation } from '../Navigation/Navigation';
+import { Loader } from 'components/Loader/Loader';
 
 export const SharedLayout = () => {
   return (
     <div>
       <Navigation />
-      <Suspense fallback={null}>
+      <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
       <Toaster
